97845827fbc910bfff8ae752cd3a82cdf7dde81c
Nate Klaiber <nate@theklaibers.com>
2019-02-14 13:30:57 -0500

    Adding a `Link` domain model

---
+++ naas/models/link.py
@@ -0,0 +1,38 @@
+"""
+
+Link
+===============
+
+This returns an instance of the Link domain model
+"""
+
+from uritemplate import URITemplate, expand
+
+class Link(object):
+	def __init__(self, attributes={}):
+		self.attributes = attributes
+
+	def title(self):
+		"""Returns the title"""
+		return self.attributes.get('title', None)
+
+	def href(self):
+		"""Returns the href"""
+		return self.attributes.get('href', None)
+
+	def rel(self):
+		"""Returns the rel"""
+		return self.attributes.get('rel', None)
+
+	def templated(self):
+		"""Returns the templated"""
+		return self.attributes.get('templated', False)
+
+	def url_for(self, args={}):
+		"""Returns the URL for this link"""
+		if self.templated() == True:
+			url = URITemplate(self.href())
+			return url.expand(args)
+		else:
+			return self.href()
+
