14e9b0706c2039af32accc87d902c4c218c0d666
Nate Klaiber <nate@theklaibers.com>
2019-02-14 13:32:11 -0500

    Adding a base Client for shorthand methods to use in Requests and Models

---
+++ naas/client.py
@@ -0,0 +1,49 @@
+"""
+
+naas.client
+===============
+
+This module contains the helper methods to manage
+API requests.
+"""
+
+import os
+import requests
+
+class Client(object):
+    @classmethod
+    def default_headers(cls):
+        """Returns the default headers for the requests"""
+        headers = {
+           'Authorization': ("Bearer %s" %(Client.access_token())),
+           'Accept': Client.default_accept_header()
+        }
+        return headers
+
+    @classmethod
+    def api_host(cls):
+        """Returns the API Host"""
+        api_host = os.environ['NAAS_API_HOST']
+        return api_host
+
+    @classmethod
+    def access_token(cls):
+        """Returns the Access Token"""
+        access_token = os.environ['NAAS_ACCESS_TOKEN']
+        return access_token
+
+    @classmethod
+    def default_accept_header(cls):
+	    header_value = 'application/vnd.naas.json; version=1'
+	    return header_value
+
+    @classmethod
+    def get(cls, url, headers={}, params={}):
+        """Execute a GET request"""
+        request_headers = { **Client.default_headers(), **headers }
+        return requests.get(url, headers=request_headers)
+
+    @classmethod
+    def routes(cls):
+        """Routes"""
+        return Client.get(Client.api_host())
