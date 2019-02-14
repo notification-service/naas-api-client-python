ab810d22a462e0751272ceb9e5225b7c0206fa62
Nate Klaiber <nate@theklaibers.com>
2019-02-14 13:31:36 -0500

    Adding a `Projects` domain model

---
+++ naas/models/projects.py
@@ -0,0 +1,15 @@
+from naas.models.project import Project
+
+class Projects(object):
+    def __init__(self, collection):
+        self.collection = list(collection)
+
+    def __iter__(self):
+        """ Implement iterator """
+        return map(lambda r: Project(r), self.collection)
+
+    def next(self):
+        if self.index == 0:
+          raise StopIteration
+        self.index = self.index - 1
+        return self.collection[self.index]
