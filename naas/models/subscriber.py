f59a218ac55ef2062e13bf0976d739d5996dd1f9
orache arthur <arthur.orache@gmail.com>
2019-03-27 20:33:31 +0300

    Add subscriber model

---
+++ naas/models/subscriber.py
@@ -0,0 +1,48 @@
+"""
+
+Subscriber
+===============
+
+This returns an instance of the Subscriber domain model
+"""
+
+
+class Subscriber(object):
+    def __init__(self, attributes={}):
+        self.attributes = attributes
+
+    def id(self):
+        """Returns the subscriber id"""
+        return self.attributes.get('id', None)
+
+    def first_name(self):
+        """Returns the subscriber first_name"""
+        return self.attributes.get('first_name', None)
+
+    def last_name(self):
+        """Returns the subscriber last_name"""
+        return self.attributes.get('last_name', None)
+
+    def full_name(self):
+        """Returns the subscriber full_name"""
+        return f'{self.first_name} {self.last_name}'
+
+    def email(self):
+        """Returns the subscriber email"""
+        return self.attributes.get('email', None)
+
+    def created_at(self):
+        """Returns the created at timestamp value"""
+        return self.attributes.get('created_at', None)
+
+    def updated_at(self):
+        """Returns the updated at timestamp value"""
+        return self.attributes.get('updated_at', None)
+
+    def links_attributes(self):
+        """Returns the links collection attributes"""
+        return self.attributes.get('links', [])
+
+    def subscriber_email_addresses_attributes(self):
+        """Returns the associated email addresses attributes"""
+        return self.attributes.get('subscriber_email_addreses', [])
