e21f0fefca6defb3f8590541febc24060ee199c3
Muhwezi Allan <allan.muhwezi@andela.com>
2019-04-01 22:11:44 +0300

    convert the indentation from tabs to spaces

--- naas/models/link.py
+++ naas/models/link.py
@@ -2,36 +2,36 @@ from uritemplate import URITemplate, expand
 
 
 class Link(object):
-	"""
+    """
 
-	Link
-	===============
+    Link
+    ===============
 
-	This returns an instance of the Link domain model
-	"""
-	def __init__(self, attributes={}):
-		self.attributes = attributes
+    This returns an instance of the Link domain model
+    """
+    def __init__(self, attributes={}):
+        self.attributes = attributes
 
-	def title(self):
-		"""Returns the title"""
-		return self.attributes.get('title', None)
+    def title(self):
+        """Returns the title"""
+        return self.attributes.get('title', None)
 
-	def href(self):
-		"""Returns the href"""
-		return self.attributes.get('href', None)
+    def href(self):
+        """Returns the href"""
+        return self.attributes.get('href', None)
 
-	def rel(self):
-		"""Returns the rel"""
-		return self.attributes.get('rel', None)
+    def rel(self):
+        """Returns the rel"""
+        return self.attributes.get('rel', None)
 
-	def templated(self):
-		"""Returns the templated"""
-		return self.attributes.get('templated', False)
+    def templated(self):
+        """Returns the templated"""
+        return self.attributes.get('templated', False)
 
-	def url_for(self, args={}):
-		"""Returns the URL for this link"""
-		if self.templated() == True:
-			url = URITemplate(self.href())
-			return url.expand(args)
-		else:
-			return self.href()
+    def url_for(self, args={}):
+        """Returns the URL for this link"""
+        if self.templated() == True:
+            url = URITemplate(self.href())
+            return url.expand(args)
+        else:
+            return self.href()
