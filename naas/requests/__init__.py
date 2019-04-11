dae848cffb1936398efcca04c679db402913d703
Muhwezi Allan <allan.muhwezi@andela.com>
2019-04-12 00:03:49 +0300

    import the requests to __init__.py

--- naas/requests/__init__.py
+++ naas/requests/__init__.py
@@ -8,3 +8,9 @@ The NAAS API Client Requests
 """
 
 from naas.requests.directory import Directory
+from naas.requests.email_notification_basics import EmailNotificationBasics
+from naas.requests.email_notification_deliveries import (
+    EmailNotificationDeliveries
+)
+from naas.requests.email_notification_statuses import EmailNotificationStatuses
+from naas.requests.email_notifications import EmailNotifications
