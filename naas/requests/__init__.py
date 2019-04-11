ec3bd7a84b59d6b6706901bf8e4439a15fe2b2d6
Muhwezi Allan <allan.muhwezi@andela.com>
2019-04-11 23:52:11 +0300

    import requests to __init__.py

--- naas/requests/__init__.py
+++ naas/requests/__init__.py
@@ -7,4 +7,11 @@ The NAAS API Client Requests
 
 """
 
+from naas.requests.accounts import Accounts
+from naas.requests.account_settings import AccountSettings
+from naas.requests.account_smtp_settings import AccountSmtpSettings
+from naas.requests.campaign_email_templates import CampaignEmailTemplates
 from naas.requests.directory import Directory
+from naas.requests.projects import Projects
+from naas.requests.subscriber_email_addresses import SubscriberEmailAddresses
+from naas.requests.subscribers import Subscriber
