2b16732745a839a699f0674de55bd6b8386cb574
Muhwezi Allan <allan.muhwezi@andela.com>
2019-05-10 22:47:13 +0300

    add tests for account settings requests

---
+++ tests/test_requests_account_settings.py
@@ -0,0 +1,6 @@
+from naas.requests.subscribers import Subscriber
+from naas.requests.subscriber_email_addresses import SubscriberEmailAddresses
+from tests import BaseTestCase
+
+
+class TestRequestsAccountSettings(BaseTestCase):
