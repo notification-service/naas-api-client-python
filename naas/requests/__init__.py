884ea11a74e6d2d7baa3aa9513c5e1eabad1b544
orache arthur <arthur.orache@gmail.com>
2019-08-27 23:54:07 +0300

    Add project subscriber profile requests

--- naas/requests/__init__.py
+++ naas/requests/__init__.py
@@ -18,6 +18,7 @@ from naas.requests.projects import Projects
 from naas.requests.project_properties import ProjectProperties
 from naas.requests.project_subscribers import ProjectSubscribers
 from naas.requests.project_subscriber_properties import ProjectSubscriberProperties
+from naas.requests.project_subscriber_profiles import ProjectSubscriberProfiles
 from naas.requests.subscriber_email_addresses import SubscriberEmailAddresses
 from naas.requests.subscribers import Subscriber
 from naas.requests.email_notification_basics import EmailNotificationBasics
