ca270f44e51325aace16775851a1e49016451d17
Nate Klaiber <nate@theklaibers.com>
2020-02-27 13:39:57 -0500

    Fix the updating project property by project id

--- naas/requests/project_properties.py
+++ naas/requests/project_properties.py
@@ -67,7 +67,7 @@ class ProjectProperties:
         return request
 
     @staticmethod
-    def update_by_project_id(project_id, params=None):
+    def update_by_project_id(project_id, _id, params=None):
         """
         Update an existing record
 
@@ -84,9 +84,9 @@ class ProjectProperties:
         headers = {
             'Content-Type': 'application/json'
         }
-        rel = Client.rel_for('rels/project-properties')
+        rel = Client.rel_for('rels/project-property')
         route = Client.routes().route_for(rel)
-        url = route.url_for(args={**params, **{'project_id': project_id}})
+        url = route.url_for(args={**params, **{'project_id': project_id, 'id': _id}})
         request = Client.put(
             url, headers=headers, data=json.dumps(request_body))
         return request
